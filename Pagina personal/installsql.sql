e555d309ced3e7201fef41c17f5821630d61ec27
Sermega <ser.matossi@gmail.com>
2024-12-01 11:12:00 +0100

    Añadidos wordpress, mediawiki y pagina personal

---
+++ Pagina personal/installsql.sql
@@ -0,0 +1,8 @@
+CREATE TABLE `proyectos` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(20) NOT NULL,
+  `image` varchar(100) NOT NULL,
+  `description` varchar(120) NOT NULL,
+  PRIMARY KEY (`id`)
+);
+
